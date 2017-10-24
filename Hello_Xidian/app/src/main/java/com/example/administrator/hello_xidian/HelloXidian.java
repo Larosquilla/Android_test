6de0d37fbf821083eaabb4eb3bbd87e058102d93
larosquilla <linchariny@gmail.com>
2017-10-24 19:55:24 +0800

    first step

---
+++ Hello_Xidian/app/src/main/java/com/example/administrator/hello_xidian/HelloXidian.java
@@ -0,0 +1,13 @@
+package com.example.administrator.hello_xidian;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HelloXidian extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_xidian);
+    }
+}
